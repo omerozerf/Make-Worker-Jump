aad018458992107ad3a891ea57f2c921fdf81226
omer faruk Ozer <ozeromerfaruk@gmail.com>
2022-11-19 02:27:10 +0300

    add scripts

---
+++ Assets/Scripts/Obstacles/ObstacleMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        Movement();
+        ObstacleAreaLimit();
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    private void ObstacleAreaLimit()
+    {
+        if (transform.position.x < -5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/ObstaclesSpawner.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObstaclesSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstacles;
+    [SerializeField] private float spawnRatePerSecond;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        var waitTime = 1f / spawnRatePerSecond;
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            
+            SpawnRandomObstacle();
+        }
+    }
+    
+    private void SpawnRandomObstacle()
+    {
+        var randomObstacleIndex = Random.Range(0, obstacles.Length);
+        var randomObstaclePrefab = obstacles[randomObstacleIndex];
+        var newPosition = transform.position + new Vector3(22f, 0f, 0f);
+        Instantiate(randomObstaclePrefab, newPosition, randomObstaclePrefab.transform.rotation, transform);
+    }
+    
+}
