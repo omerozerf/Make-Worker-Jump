2e7f2f5a93e7522b08001cbeb4540b21cc20c04e
omer faruk Ozer <ozeromerfaruk@gmail.com>
2022-11-19 01:26:23 +0300

    background movement

--- Assets/Scripts/Background/BackgroundMovement.cs
+++ Assets/Scripts/Background/BackgroundMovement.cs
@@ -1,20 +1,37 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundMovement : MonoBehaviour
 {
 
     [SerializeField] private float speed;
+    [SerializeField] private Transform[] transforms;
+    
 
     private void Update()
     {
         XMovement();
+        AreaLimit();
     }
 
     private void XMovement()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        foreach (var subTransform in transforms)
+        {
+            var deltaPosition = Vector3.left * speed * Time.deltaTime;
+            subTransform.position = subTransform.position + deltaPosition;
+        }
+    }
+
+    private void AreaLimit()
+    {
+        var transformLenght = Mathf.Abs(transforms[0].position.x - transforms[1].position.x);
+        
+        foreach (var subTransform in transforms)
+        {
+            if (subTransform.position.x < -68f)
+            {
+                subTransform.position = subTransform.position + Vector3.right * (transformLenght * 2f);
+            }
+        }
     }
 }
